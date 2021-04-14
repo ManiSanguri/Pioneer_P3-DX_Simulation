dcdafb7636ec898d314828e249bec9c40db12b39
Mani Sanguri <68176842+ManiSanguri@users.noreply.github.com>
2021-04-14 14:52:25 +0530

    Add files via upload

--- First_Scene_Matlab_Code.m
+++ First_Scene_Matlab_Code.m
@@ -1,16 +1,17 @@
+ % import v-rep
  vrep=remApi('remoteApi');
  vrep.simxFinish(-1);
  clientID=vrep.simxStart('127.0.0.1',19999,true,true,5000,5);
  if (clientID>-1)
      disp('Connected');
      
-     [returnCode,detectionState,detectedPoint,~,~]=vrep.simxReadProximitySensor(clientID,Front_Sensor,vrep.simx_opmode_streaming);
-     
      % Handle
      
      [returnCode,Left_Motor] = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor',vrep.simx_opmode_blocking);
      [returnCode,Front_Sensor] = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor5',vrep.simx_opmode_blocking);
      [returnCode,camera] = vrep.simxGetObjectHandle(clientID,'Vision_sensor',vrep.simx_opmode_blocking);  
+
+     [returnCode,detectionState,detectedPoint,~,~]=vrep.simxReadProximitySensor(clientID,Front_Sensor,vrep.simx_opmode_streaming);
      
      % Other Code
      [returnCode,resolution,image] = vrep.simxGetVisionSensorImage2(clientID,camera,0,vrep.simx_opmode_streaming);   
